933b47ff867d70120eeaa38d5c70ccba05bc7c58
Glitch (c2is---p4) <none>
2022-05-12 06:35:29 +0000

    🀄🧀 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Broken Mirrors");
+
+
+
+// var break2 = document.getElementsByClassName("break2");
+
+// break2.onclick = function() { opacity: 100 };
+
+function toggleText() {
+  var text = document.getElementById("demo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+
+
+var sample = document.getElementById("foobar");
+sample.play();
+
+
+
+// const bg1 = document.getElementById("bg-1");
+
+// const btn = document.getElementById("btnAdd");
+
+// btn.addEventListener("click", function(){
+//     const div = document.createElement("div");
+//     div.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16)
+//     bg1.appendChild(div);
+// });
